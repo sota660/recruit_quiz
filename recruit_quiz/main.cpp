a1705e9f4ad619b32ef0d8d89d25a641b706cf59
蒼大 谷岡 <souko8611@gmail.com>
2025-04-23 15:17:53 +0900

    問題数を増やす

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+//問題文と答えをまとめる構造体
+struct Question
+{
+	string q;	//問題文
+	int a;		//答え
+};
 
 int main()
 {
-	cout << "[リクルート試験対策クイズ]\n";
-	cout << "13×(-5)の答えは？\n";
+	Question questions[] = {
+		{ "13×(-5)", 13 * -5 },
+		{"(-21)÷(-3)", -21 / -3 },
+		{ "7-(4+2)÷2", 7 - (4 + 2) / 2},
+	};
 
-	int answer;
-	cin >> answer;
-	if (answer == 13 * -5) {
-		cout << "正解！\n";
-	}
-	else {
-		cout << "間違い！正解は" << 13 * -5 << "\n";
-	}
+	cout << "[リクルート試験対策クイズ]\n";
+	//cout << "13×(-5)の答えは？\n";
+
+	for (const auto& e : questions) {
+		cout << e.q << "の答えは？\n";
+
+		int answer;
+		cin >> answer;
+		//if (answer == 13 * -5) {
+		if (answer == e.a) {
+			cout << "正解！\n";
+		}
+		else {
+			//cout << "間違い！正解は" << 13 * -5 << "\n";
+			cout << "間違い！正解は" << e.a << "\n";
+		}
+	} // for questions
 }
